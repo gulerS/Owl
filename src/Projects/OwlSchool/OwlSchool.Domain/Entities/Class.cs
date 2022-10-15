3d6de912065d40ec3da3cb3751740e87f608adc7
Sercan <sercanguler@live.com>
2022-10-15 23:24:31 +0300

    code-first add first migrartion

---
+++ src/Projects/OwlSchool/OwlSchool.Domain/Entities/Class.cs
@@ -0,0 +1,19 @@
+using Core.Persistence.Repositories;
+
+namespace OwlSchool.Domain.Entities;
+
+public class Class : Entity
+{
+    public Class()
+    {
+    }
+
+    public Class(string name, int floor) : this()
+    {
+        Name = name;
+        Floor = floor;
+    }
+
+    public string Name { get; set; }
+    public int Floor { get; set; }
+}
---
+++ src/Projects/OwlSchool/OwlSchool.Persistence/Migrations/20221015200956_Initial.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OwlSchool.Persistence.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Class",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Floor = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Class", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Class",
+                columns: new[] { "Id", "Floor", "Name" },
+                values: new object[] { 1, 1, "Science 1" });
+
+            migrationBuilder.InsertData(
+                table: "Class",
+                columns: new[] { "Id", "Floor", "Name" },
+                values: new object[] { 2, 2, "Science 2" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Class");
+        }
+    }
+}
